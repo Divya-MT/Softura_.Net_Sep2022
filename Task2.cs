78524d5127f7a9cecb8800623aa8993e7e64c401
Divya-MT <112896433+Divya-MT@users.noreply.github.com>
2022-09-26 20:13:42 +0530

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FifteenthDayClasCSharp
+{
+    internal class Task2
+    {
+        public void insData(int BussinuessEntityID, string passwordsalt, DateTime date)
+        {
+            SqlConnection con = new SqlConnection("data source = ykg-pc; database=AdventureWorks2019; user id=sa; password=p@ssword");
+            SqlCommand cmd = new SqlCommand("sp_Person.Password", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@BussinuessEntityID", SqlDbType.Int).Value = BussinuessEntityID;
+            cmd.Parameters.AddWithValue("@passwordsalt", SqlDbType.NVarChar).Value = passwordsalt;
+            cmd.Parameters.AddWithValue("@ModifiedDate", SqlDbType.DateTime).Value = date;
+
+
+
+
+            con.Open();
+           // cmd.ExecuteNonQuery();
+            con.Close();
+
+            Console.WriteLine("inserted");
+
+        }
+        public static void Main()
+        {
+          Task2 task2 = new Task2();
+            task2.insData( 20778, "7iy/muc=", DateTime.Now );
+        }
+
+
+    }
+}
+    
+
