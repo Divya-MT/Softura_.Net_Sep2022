c4118e96cc1f3abf1706c6695953c1107fac2b86
Divya-MT <112896433+Divya-MT@users.noreply.github.com>
2022-10-05 18:50:28 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace EmployeeApp.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+   
+
+    public static List<Employee> GetAllEmployee()
+    {
+        List<Employee> employees = new()
+        {
+            new Employee(){ EmployeeId =1001,FirstName="Banu",LastName="Priya",Gender="Female"},
+            new Employee(){ EmployeeId =1002,FirstName="Divya",LastName="Bharathi",Gender="Female"},
+            new Employee(){ EmployeeId =1003,FirstName="Deepika",LastName="sri",Gender="Female"},
+            new Employee(){ EmployeeId =1003,FirstName="Bharathi",LastName="Arivu",Gender="Female"},
+            new Employee(){ EmployeeId =1004,FirstName="siva",LastName="Priya",Gender="Female"},
+        };
+        return employees;
+    }
+        public static Employee GetEmpInfo()
+        {
+            Employee employee1 = new Employee()
+            {
+                EmployeeId = 1004,
+                FirstName = "siva",
+                LastName = "Priya",
+                Gender = "Female"
+            };
+            return employee1;
+        }
+
+    }
+}
---
+++ EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EmployeeApp.Models;
+
+namespace EmployeeApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult GetEmployee()
+        {
+            List<Employee> liemployees = Employee.GetAllEmployee();
+            return View(liemployees);
+        }
+
+        public IActionResult GetOneEmployee()
+        {
+            Employee employee1 = new Employee()
+            {
+                EmployeeId = 1004,
+                FirstName = "siva",
+                LastName = "Priya",
+                Gender = "Female"
+            };
+            return View(employee1);
+        }
+        public IActionResult AddEmploye()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+       
+    }
+}
